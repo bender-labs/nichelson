994b1f78064c1d898e42ef85cc7282410ede5ee2
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-02-25 11:24:45 +0100

    Bump version to 0.0.17
    ## [0.0.17] - 2021-02-25
    
    ### Security
    - Bumps Netezos version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.0.17] - 2021-02-25
 
 ### Security
 - Bumps Netezos version
@@ -14,5 +14,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 - Initial release
-[Unreleased]: https://github.com/bender-labs/nichelson/compare/v0.0.16...HEAD
+[Unreleased]: https://github.com/bender-labs/nichelson/compare/v0.0.17...HEAD
+[0.0.17]: https://github.com/bender-labs/nichelson/compare/v0.0.16...v0.0.17
 [0.0.16]: https://github.com/bender-labs/nichelson/releases/tag/v0.0.16
--- src/Nichelson/AssemblyInfo.fs
+++ src/Nichelson/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Nichelson")>]
 [<assembly: AssemblyProductAttribute("Nichelson")>]
-[<assembly: AssemblyVersionAttribute("0.0.16")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-02-09T00:00:00.0000000+01:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.16")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.16")>]
+[<assembly: AssemblyVersionAttribute("0.0.17")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-02-25T00:00:00.0000000+01:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.17")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.17")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","2375772620b4cb14cafd95b60827a762cbe2da5d")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","af6d179979fbdabf7f8fa588ba1801a4225c86b2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Nichelson"
     let [<Literal>] AssemblyProduct = "Nichelson"
-    let [<Literal>] AssemblyVersion = "0.0.16"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-02-09T00:00:00.0000000+01:00"
-    let [<Literal>] AssemblyFileVersion = "0.0.16"
-    let [<Literal>] AssemblyInformationalVersion = "0.0.16"
+    let [<Literal>] AssemblyVersion = "0.0.17"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-02-25T00:00:00.0000000+01:00"
+    let [<Literal>] AssemblyFileVersion = "0.0.17"
+    let [<Literal>] AssemblyInformationalVersion = "0.0.17"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "2375772620b4cb14cafd95b60827a762cbe2da5d"
+    let [<Literal>] AssemblyMetadata_GitHash = "af6d179979fbdabf7f8fa588ba1801a4225c86b2"
