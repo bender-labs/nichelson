5bf1f0f6d837adcb580ee113abc5cf09a506652b
Jean-Baptiste Dusseaut <jb@benderlabs.io>
2021-02-25 16:09:43 +0100

    chore: bumps assemblyinfo

--- tests/Nichelson.Test/AssemblyInfo.fs
+++ tests/Nichelson.Test/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Nichelson.Test")>]
 [<assembly: AssemblyProductAttribute("Nichelson")>]
-[<assembly: AssemblyVersionAttribute("0.0.17")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-02-25T00:00:00.0000000+01:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.17")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.17")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","af6d179979fbdabf7f8fa588ba1801a4225c86b2")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","9246151aea4c726851943861cc7a60a09396a515")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Nichelson.Test"
     let [<Literal>] AssemblyProduct = "Nichelson"
-    let [<Literal>] AssemblyVersion = "0.0.17"
+    let [<Literal>] AssemblyVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-02-25T00:00:00.0000000+01:00"
-    let [<Literal>] AssemblyFileVersion = "0.0.17"
-    let [<Literal>] AssemblyInformationalVersion = "0.0.17"
+    let [<Literal>] AssemblyFileVersion = "0.1.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "af6d179979fbdabf7f8fa588ba1801a4225c86b2"
+    let [<Literal>] AssemblyMetadata_GitHash = "9246151aea4c726851943861cc7a60a09396a515"
